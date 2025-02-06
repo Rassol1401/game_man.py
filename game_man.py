20f62fcd300183416274c990ee4c7e9aa252edc6
Rassol1401 <rassolenkomaksim1@gmail.com>
2025-02-06 23:43:33 +0300

    Add files via upload

--- game_man.py
+++ game_man.py
@@ -5,71 +5,66 @@ import os
 
 
 def replace(skill):
- for letter in skill:
-    skill=skill.replace(letter,alphabet.get(letter))
- return skill
-
+    for letter in skill:
+        skill = skill.replace(letter, alphabet.get(letter))
+    return skill
 
 
 if __name__ == '__main__':
     fake = Faker("ru_RU")
-    alphabet ={
-    'а':'а͠', 'б': 'б̋', 'в': 'в͒͠',
-    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-    ' ':' '
+    alphabet = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' ',
     }
-    list_lines=[
-    'Стремительный прыжок',
-    'Электрический встрел',
-    'Ледяной удар',
-    'Стремительный удар',
-    'Кислотный взгляд',
-    'Тайный побег',
-    'Ледяной выстрел',
-    'Огненный заряд'
+    list_lines = [
+        'Стремительный прыжок',
+        'Электрический встрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд',
     ]
-    scr = os.path.join('c:','image')
+    scr = os.path.join('c:', 'image')
     os.makedirs(scr, mode=0o777, exist_ok=True)
     for bum in range(10):
-        list_lines_random = random.sample(list_lines,3)
-        renamed_skills=[replace(skill) for skill in list_lines_random]
-        context={
-        "first_name":fake.last_name_male(),
-        "last_name":fake.first_name_male(),
-        "job":fake.job(),
-        "town":fake.city(),
-        "strength":random.randint(3,18),
-        "agility":random.randint(3,18),
-        "endurance":random.randint(3,18),
-        "intelligence":random.randint(3,18),
-        "luck":random.randint(3,18),
-        "skill_1":renamed_skills[0],
-        "skill_2":renamed_skills[1],
-        "skill_3":renamed_skills[2],
+        list_lines_random = random.sample(list_lines, 3)
+        renamed_skills = [replace(skill) for skill in list_lines_random]
+        context = {
+            "first_name": fake.last_name_male(),
+            "last_name": fake.first_name_male(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": renamed_skills[0],
+            "skill_2": renamed_skills[1],
+            "skill_3": renamed_skills[2],
         }
-        file_operations.render_template("result.svg",f"{scr}/charsheet_{bum}.svg",context)
-
-
-
-
+        file_operations.render_template("result.svg", f"{scr}/charsheet_{bum}.svg", context)
 
